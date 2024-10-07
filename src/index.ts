8e932bb0cc51a2a3e95de2ee3a5e37c057c36082
pBread <4473570+pBread@users.noreply.github.com>
2024-10-07 11:40:34 -0600

    it's working

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,40 @@
 import dotenv from "dotenv-flow";
 import express from "express";
+import ExpressWs from "express-ws";
 import type { CallStatus } from "./twilio-types";
 
 dotenv.config();
 
-const app = express();
+const { app } = ExpressWs(express());
 app.use(express.urlencoded({ extended: true })).use(express.json());
 
 /****************************************************
  Twilio Voice Webhooks
 ****************************************************/
-app.post("/incoming-call", async (req, res) => {
+app.use("/incoming-call", async (req, res) => {
+  console.log("/incoming-call", req.body);
+
+  const CallSid = req.body.CallSid as string;
+
   res.status(200);
   res.type("text/xml");
 
+  // start transcription service
+  // say hello
+  // connect the call to a media stream so the call stays open
+
   res.end(`
-    <Response>
-      <Connect>
-        <Transcription statusCallbackUrl="https://${process.env.HOSTNAME}/transcript-status-callback"/> 
-      </Connect>
-    </Response>
-    `);
+      <Response>
+        <Start>
+          <Transcription track="both_tracks" statusCallbackUrl="https://${process.env.HOSTNAME}/transcript-status-callback" name="real-time-transcript" > </Transcription>
+        </Start>
+        
+        <Say> Hello, this is a real-time transcription demo. Please speak </Say>
+        <Connect>
+            <Stream url="wss://${process.env.HOSTNAME}/media-stream/${CallSid}" />
+        </Connect>
+      </Response>
+      `);
 });
 
 app.post("/call-status-update", async (req, res) => {
@@ -31,10 +45,28 @@ app.post("/call-status-update", async (req, res) => {
   res.status(200).send();
 });
 
+app.post("/primary-handler-fails", async (req, res) => {
+  console.log(`/primary-handler-fails`, req.body);
+
+  res.status(200).send();
+});
+
+app.ws("/media-stream/:callSid", (ws, req) => {
+  console.log("incoming websocket");
+
+  ws.on("message", (data) => {
+    const msg = JSON.parse(data.toString());
+
+    if (msg.event === "media") return;
+
+    console.log(msg.event);
+  });
+});
+
 /****************************************************
  Transcription Handler
 ****************************************************/
-app.post(`/transcript-status-callback`, async (req, res) => {
+app.use(`/transcript-status-callback`, async (req, res) => {
   console.log(`/transcript-status-callback`, req.body);
 
   res.status(200).send();
