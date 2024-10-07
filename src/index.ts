a28a0ab5ed5f1afd221fb6a845bd459f6ee3a0fc
pBread <4473570+pBread@users.noreply.github.com>
2024-10-07 11:11:13 -0600

    added initial webhooks. comitting to cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,49 @@
-console.log("hello world");
+import dotenv from "dotenv-flow";
+import express from "express";
+import type { CallStatus } from "./twilio-types";
+
+dotenv.config();
+
+const app = express();
+app.use(express.urlencoded({ extended: true })).use(express.json());
+
+/****************************************************
+ Twilio Voice Webhooks
+****************************************************/
+app.post("/incoming-call", async (req, res) => {
+  res.status(200);
+  res.type("text/xml");
+
+  res.end(`
+    <Response>
+      <Connect>
+        <Transcription statusCallbackUrl="https://${process.env.HOSTNAME}/transcript-status-callback"/> 
+      </Connect>
+    </Response>
+    `);
+});
+
+app.post("/call-status-update", async (req, res) => {
+  const status = req.body.CallStatus as CallStatus;
+
+  console.log(`/call-status-update ${status}`);
+
+  res.status(200).send();
+});
+
+/****************************************************
+ Transcription Handler
+****************************************************/
+app.post(`/transcript-status-callback`, async (req, res) => {
+  console.log(`/transcript-status-callback`, req.body);
+
+  res.status(200).send();
+});
+
+/****************************************************
+ Start Server
+****************************************************/
+const port = process.env.PORT || "3000";
+app.listen(port, () => {
+  console.log(`server running on http://localhost:${port}`);
+});
---
+++ src/twilio-types.ts
@@ -0,0 +1,1 @@
+export type CallStatus = "completed" | "initializing" | "started" | "error";
