e1c773ae7cc49c1e694a345125d90b362ff17029
pBread <4473570+pBread@users.noreply.github.com>
2024-10-07 12:53:21 -0600

    updated env for sync

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,10 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     HOSTNAME: string;
+
+    TWILIO_ACCOUNT_SID: string;
+    TWILIO_AUTH_TOKEN: string;
+
+    TWILIO_SYNC_SVC_SID: string;
   }
 }
